e4d30010f340a78cab4f69652af1107e39b43f98
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2023-10-06 08:33:54 +0330

    updated

--- recaptcha_bypass.py
+++ recaptcha_bypass.py
@@ -8,11 +8,24 @@ from ultralytics import YOLO
 import numpy as np
 from PIL import Image
 import cv2
-model = YOLO("yolov8x.pt")
+from pathlib import Path
+import random
+from datetime import datetime
+from os import listdir, remove
+
+current_directory = Path().absolute()
+models_directory = current_directory.joinpath("models")
+images_directory = current_directory.joinpath("images")
+
+yolo_model = YOLO(models_directory.joinpath("yolov8x.pt"))
+crosswalk_model = YOLO(models_directory.joinpath("crosswalk.pt"))
+yolo_oiv7_model = YOLO(models_directory.joinpath("yolov8x-oiv7.pt"))
+yolo_seg_model = YOLO(models_directory.joinpath("yolov8x-seg.pt"))
+
 try:
-    image = Image.open("0.png")
+    image = Image.open(images_directory.joinpath("0.png"))
     image = np.asarray(image)
-    model.predict(image)
+    crosswalk_model.predict(image)
 except:
     ...
 
@@ -39,6 +52,8 @@ def get_target_num(driver):
         return 1
     elif re.search(r"bus", target.text) != None:
         return 5
+    elif re.search(r"tractor", target.text) != None:
+        return 7
     elif re.search(r"boat", target.text) != None:
         return 8
     elif re.search(r"car", target.text) != None:
@@ -49,14 +64,31 @@ def get_target_num(driver):
         return 3
     elif re.search(r"traffic", target.text) != None:
         return 9
+    elif re.search(r"crosswalk", target.text) != None:
+        return 1001
+    elif re.search(r"stair", target.text) != None:
+        return 1002
+    elif re.search(r"taxi", target.text) != None:
+        return 1003
     else:
         return 1000
 
 
-def get_answers(target_num):
-    image = Image.open("0.png")
+def get_answers(target_num,timestamp):
+    image = Image.open(images_directory.joinpath(f"0-{timestamp}.png"))
     image = np.asarray(image)
-    result = model.predict(image)
+
+    if target_num == 1001:
+        result = crosswalk_model.predict(image)
+        target_num = 0
+    elif target_num == 1002:
+        result = yolo_oiv7_model.predict(image)
+        target_num = 489
+    elif target_num == 1003:
+        result = yolo_oiv7_model.predict(image)
+        target_num = 522
+    else:
+        result = yolo_model.predict(image)
 
     target_index = []
     count = 0
@@ -115,9 +147,9 @@ def get_all_captcha_img_urls(driver):
     return img_urls
 
 
-def download_img(name, url):
+def download_img(name, url,timestamp):
     response = requests.get(url, stream=True)
-    with open(f'{name}.png', 'wb') as out_file:
+    with open(images_directory.joinpath(f'{name}-{timestamp}.png'), 'wb') as out_file:
         shutil.copyfileobj(response.raw, out_file)
     del response
 
@@ -147,7 +179,7 @@ def get_all_new_dynamic_captcha_img_urls(answers, before_img_urls, driver):
         return is_new, img_urls
 
 
-def paste_new_img_on_main_img(main, new, loc):
+def paste_new_img_on_main_img(main, new, loc,timestamp):
     paste = np.copy(main)
     if loc == 1:
         paste[0:100, 0:100] = new
@@ -169,7 +201,7 @@ def paste_new_img_on_main_img(main, new, loc):
         paste[200:300, 200:300] = new
 
     paste = cv2.cvtColor(paste, cv2.COLOR_RGB2BGR)
-    cv2.imwrite('0.png', paste)
+    cv2.imwrite(str(images_directory.joinpath(f'0-{timestamp}.png')), paste)
 
 
 def get_occupied_cells(vertices):
@@ -182,87 +214,154 @@ def get_occupied_cells(vertices):
 
     return sorted(list(occupied_cells))
 
+def detect_car_locations(masked_image, num_rows=4, num_columns=4, threshold=100):
+    height, width, _ = masked_image.shape
+    row_step = height // num_rows
+    col_step = width // num_columns
+    car_locations = []
 
-def get_answers_4(target_num):
-    image = Image.open("0.png")
+    for i in range(num_rows):
+        for j in range(num_columns):
+            row_start = i * row_step
+            row_end = (i + 1) * row_step
+            col_start = j * col_step
+            col_end = (j + 1) * col_step
+            region = masked_image[row_start:row_end, col_start:col_end]
+            white_pixel_count = np.sum(region > 0)
+            if white_pixel_count > threshold:
+                car_locations.append((i, j))
+
+    return car_locations
+
+def convert_to_position_indices(car_locations, num_rows=4, num_columns=4):
+    position_indices = []
+    for i, j in car_locations:
+        position = i * num_columns + j + 1
+        position_indices.append(position)
+    return position_indices
+
+def get_answers_4(target_num,timestamp):
+    image = Image.open(images_directory.joinpath(f"0-{timestamp}.png"))
     image = np.asarray(image)
-    result = model.predict(image)
-    boxes = result[0].boxes.data
+    
+    if target_num < 1000:
+        result_seg = yolo_seg_model.predict(image)
 
-    target_index = []
-    count = 0
-    for num in result[0].boxes.cls:
-        if num == target_num:
-            target_index.append(count)
-        count += 1
+        target_index = []
+        count = 0
+        for num in result_seg[0].boxes.cls:
+            if num == target_num:
+                target_index.append(count)
+            count += 1
 
-    for i in target_index:
-        target_box = boxes[i]
-        p1, p2 = (int(target_box[0]), int(target_box[1])
-                  ), (int(target_box[2]), int(target_box[3]))
-        x1, y1 = p1
-        x2, y2 = p2
+        answers = []
 
-    answers = []
-    count = 0
-    for i in target_index:
-        target_box = boxes[i]
-        p1, p2 = (int(target_box[0]), int(target_box[1])
-                  ), (int(target_box[2]), int(target_box[3]))
-        x1, y1 = p1
-        x4, y4 = p2
-        x2 = x4
-        y2 = y1
-        x3 = x1
-        y3 = y4
-        xys = [x1, y1, x2, y2, x3, y3, x4, y4]
+        for i in target_index:
+            if(result_seg[0].masks is not None):
+                mask_raw = result_seg[0].masks[i].cpu().data.numpy().transpose(1, 2, 0)
+                mask_3channel = cv2.merge((mask_raw,mask_raw,mask_raw))
+                h2, w2, c2 = result_seg[0].orig_img.shape
+                mask = cv2.resize(mask_3channel, (w2, h2))
+                hsv = cv2.cvtColor(mask, cv2.COLOR_BGR2HSV)
+                lower_black = np.array([0,0,0])
+                upper_black = np.array([0,0,1])
+                mask = cv2.inRange(mask, lower_black, upper_black)
+                mask = cv2.bitwise_not(mask)
+                masked = cv2.bitwise_and(result_seg[0].orig_img, result_seg[0].orig_img, mask=mask)
+                car_locations = detect_car_locations(masked, num_rows=4, num_columns=4, threshold=100)
+                position_indices = convert_to_position_indices(car_locations)
+                for indice in position_indices:
+                    answers.append(indice)
+        
+        answers = sorted(list(answers))
+        return list(set(answers))
+    else:
+        if target_num == 1001:
+            result = crosswalk_model.predict(image)
+            target_num = 0
+        elif target_num == 1002:
+            result = yolo_oiv7_model.predict(image)
+            target_num = 489
+        elif target_num == 1003:
+            result = yolo_oiv7_model.predict(image)
+            target_num = 522
 
-        four_cells = []
-        for i in range(4):
-            x = xys[i*2]
-            y = xys[(i*2)+1]
+        boxes = result[0].boxes.data
 
-            if x < 112.5 and y < 112.5:
-                four_cells.append(1)
-            if 112.5 < x < 225 and y < 112.5:
-                four_cells.append(2)
-            if 225 < x < 337.5 and y < 112.5:
-                four_cells.append(3)
-            if 337.5 < x <= 450 and y < 112.5:
-                four_cells.append(4)
+        target_index = []
+        count = 0
+        for num in result[0].boxes.cls:
+            if num == target_num:
+                target_index.append(count)
+            count += 1
 
-            if x < 112.5 and 112.5 < y < 225:
-                four_cells.append(5)
-            if 112.5 < x < 225 and 112.5 < y < 225:
-                four_cells.append(6)
-            if 225 < x < 337.5 and 112.5 < y < 225:
-                four_cells.append(7)
-            if 337.5 < x <= 450 and 112.5 < y < 225:
-                four_cells.append(8)
+        for i in target_index:
+            target_box = boxes[i]
+            p1, p2 = (int(target_box[0]), int(target_box[1])
+                    ), (int(target_box[2]), int(target_box[3]))
+            x1, y1 = p1
+            x2, y2 = p2
 
-            if x < 112.5 and 225 < y < 337.5:
-                four_cells.append(9)
-            if 112.5 < x < 225 and 225 < y < 337.5:
-                four_cells.append(10)
-            if 225 < x < 337.5 and 225 < y < 337.5:
-                four_cells.append(11)
-            if 337.5 < x <= 450 and 225 < y < 337.5:
-                four_cells.append(12)
+        answers = []
+        count = 0
+        for i in target_index:
+            target_box = boxes[i]
+            p1, p2 = (int(target_box[0]), int(target_box[1])
+                    ), (int(target_box[2]), int(target_box[3]))
+            x1, y1 = p1
+            x4, y4 = p2
+            x2 = x4
+            y2 = y1
+            x3 = x1
+            y3 = y4
+            xys = [x1, y1, x2, y2, x3, y3, x4, y4]
 
-            if x < 112.5 and 337.5 < y <= 450:
-                four_cells.append(13)
-            if 112.5 < x < 225 and 337.5 < y <= 450:
-                four_cells.append(14)
-            if 225 < x < 337.5 and 337.5 < y <= 450:
-                four_cells.append(15)
-            if 337.5 < x <= 450 and 337.5 < y <= 450:
-                four_cells.append(16)
-        answer = get_occupied_cells(four_cells)
-        count += 1
-        for ans in answer:
-            answers.append(ans)
-    answers = sorted(list(answers))
-    return list(set(answers))
+            four_cells = []
+            for i in range(4):
+                x = xys[i*2]
+                y = xys[(i*2)+1]
+
+                if x < 112.5 and y < 112.5:
+                    four_cells.append(1)
+                if 112.5 < x < 225 and y < 112.5:
+                    four_cells.append(2)
+                if 225 < x < 337.5 and y < 112.5:
+                    four_cells.append(3)
+                if 337.5 < x <= 450 and y < 112.5:
+                    four_cells.append(4)
+
+                if x < 112.5 and 112.5 < y < 225:
+                    four_cells.append(5)
+                if 112.5 < x < 225 and 112.5 < y < 225:
+                    four_cells.append(6)
+                if 225 < x < 337.5 and 112.5 < y < 225:
+                    four_cells.append(7)
+                if 337.5 < x <= 450 and 112.5 < y < 225:
+                    four_cells.append(8)
+
+                if x < 112.5 and 225 < y < 337.5:
+                    four_cells.append(9)
+                if 112.5 < x < 225 and 225 < y < 337.5:
+                    four_cells.append(10)
+                if 225 < x < 337.5 and 225 < y < 337.5:
+                    four_cells.append(11)
+                if 337.5 < x <= 450 and 225 < y < 337.5:
+                    four_cells.append(12)
+
+                if x < 112.5 and 337.5 < y <= 450:
+                    four_cells.append(13)
+                if 112.5 < x < 225 and 337.5 < y <= 450:
+                    four_cells.append(14)
+                if 225 < x < 337.5 and 337.5 < y <= 450:
+                    four_cells.append(15)
+                if 337.5 < x <= 450 and 337.5 < y <= 450:
+                    four_cells.append(16)
+            answer = get_occupied_cells(four_cells)
+            count += 1
+            for ans in answer:
+                answers.append(ans)
+        answers = sorted(list(answers))
+        return list(set(answers))
 
 
 def solve_recaptcha(driver):
@@ -273,7 +372,10 @@ def solve_recaptcha(driver):
 
     go_to_recaptcha_iframe2(driver)
 
-    count_solve = 0
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    random_num = random.randint(100000, 999999)
+    timestamp = f"{timestamp}_{random_num}"
+
     while True:
         try:
             while True:
@@ -290,8 +392,8 @@ def solve_recaptcha(driver):
                 elif "squares" in title_wrapper.text:
                     print("Square captcha found....")
                     img_urls = get_all_captcha_img_urls(driver)
-                    download_img(0, img_urls[0])
-                    answers = get_answers_4(target_num)
+                    download_img(0, img_urls[0],timestamp)
+                    answers = get_answers_4(target_num,timestamp)
                     if len(answers) >= 1 and len(answers) < 16:
                         captcha = "squares"
                         break
@@ -300,18 +402,21 @@ def solve_recaptcha(driver):
                 elif "none" in title_wrapper.text:
                     print("found a 3x3 dynamic captcha")
                     img_urls = get_all_captcha_img_urls(driver)
-                    download_img(0, img_urls[0])
-                    answers = get_answers(target_num)
-                    if len(answers) > 2:
-                        captcha = "dynamic"
-                        break
+                    if len(set(list(img_urls))) == 1:
+                        download_img(0, img_urls[0],timestamp)
+                        answers = get_answers(target_num,timestamp)
+                        if len(answers) > 2:
+                            captcha = "dynamic"
+                            break
+                        else:
+                            reload.click()
                     else:
                         reload.click()
                 else:
                     print("found a 3x3 one time selection captcha")
                     img_urls = get_all_captcha_img_urls(driver)
-                    download_img(0, img_urls[0])
-                    answers = get_answers(target_num)
+                    download_img(0, img_urls[0],timestamp)
+                    answers = get_answers(target_num,timestamp)
                     if len(answers) > 2:
                         captcha = "selection"
                         break
@@ -338,15 +443,15 @@ def solve_recaptcha(driver):
                     new_img_index_urls
 
                     for index in new_img_index_urls:
-                        download_img(index+1, img_urls[index])
+                        download_img(index+1, img_urls[index],timestamp)
                     while True:
                         try:
                             for answer in answers:
-                                main_img = Image.open("0.png")
-                                new_img = Image.open(f"{answer}.png")
+                                main_img = Image.open(images_directory.joinpath(f"0-{timestamp}.png"))
+                                new_img = Image.open(images_directory.joinpath(f"{answer}-{timestamp}.png"))
                                 location = answer
                                 paste_new_img_on_main_img(
-                                    main_img, new_img, location)
+                                    main_img, new_img, location,timestamp)
                             break
                         except:
                             while True:
@@ -359,9 +464,9 @@ def solve_recaptcha(driver):
                                 new_img_index_urls.append(answer-1)
 
                             for index in new_img_index_urls:
-                                download_img(index+1, img_urls[index])
+                                download_img(index+1, img_urls[index],timestamp)
 
-                    answers = get_answers(target_num)
+                    answers = get_answers(target_num,timestamp)
 
                     if len(answers) >= 1:
                         for answer in answers:
@@ -379,13 +484,19 @@ def solve_recaptcha(driver):
             verify.click()
 
             try:
-                go_to_recaptcha_iframe1(driver)
+                # //span[@id="recaptcha-anchor"]
                 WebDriverWait(driver, 2).until(
-                    EC.presence_of_element_located((By.XPATH, '//span[contains(@class, "recaptcha-checkbox-checked")]')))
+                    EC.presence_of_element_located((By.XPATH, '//button[@id="recaptcha-verify-button" and not(contains(@class, "rc-button-default-disabled"))]')))
+            except:
                 print("solved")
+
+                list_images_directory = listdir(images_directory)
+                for image in list_images_directory:
+                    if re.search(timestamp, image) != None:
+                        remove(images_directory.joinpath(image))
+
                 driver.switch_to.default_content()
                 break
-            except:
-                go_to_recaptcha_iframe2(driver)
+
         except Exception as e:
             print(e)
