0ab12cf5ff8f305159cfa3b60ed80b4446ec29cc
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2023-10-21 16:57:20 +0330

    The code was cleaned

--- recaptcha_bypass.py
+++ recaptcha_bypass.py
@@ -1,69 +1,64 @@
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-import shutil
-import requests
-import re
-from ultralytics import YOLO
-import numpy as np
-from PIL import Image
-import cv2
-from pathlib import Path
-import random
-from datetime import datetime
 from os import listdir, remove
+from datetime import datetime
+from ultralytics import YOLO
+from pathlib import Path
+from PIL import Image
+import numpy as np
+import requests
+import random
+import shutil
+import cv2
+import re
 
 current_directory = Path().absolute()
-models_directory = current_directory.joinpath("models")
-images_directory = current_directory.joinpath("images")
+models_directory  = current_directory.joinpath("models")
+images_directory  = current_directory.joinpath("images")
 
-yolo_model = YOLO(models_directory.joinpath("yolov8x.pt"))
-crosswalk_model = YOLO(models_directory.joinpath("crosswalk.pt"))
-yolo_oiv7_model = YOLO(models_directory.joinpath("yolov8x-oiv7.pt"))
-yolo_seg_model = YOLO(models_directory.joinpath("yolov8x-seg.pt"))
+# Define YOLO models
+yolo_models = {
+    "yolov8x"     : YOLO(models_directory.joinpath("yolov8x.pt")),
+    "crosswalk"   : YOLO(models_directory.joinpath("crosswalk.pt")),
+    "yolov8x-oiv7": YOLO(models_directory.joinpath("yolov8x-oiv7.pt")),
+    "yolov8x-seg" : YOLO(models_directory.joinpath("yolov8x-seg.pt"))
+}
 
+# Try loading an image with a model
 try:
     image = Image.open(images_directory.joinpath("0.png"))
     image = np.asarray(image)
-    crosswalk_model.predict(image)
+    yolo_models["yolov8x"].predict(image)
 except:
-    ...
+    pass
 
 
-def go_to_recaptcha_iframe1(driver):
+def go_to_recaptcha_iframe(driver, iframe_xpath):
     driver.switch_to.default_content()
-    recaptcha_iframe1 = WebDriverWait(driver=driver, timeout=10).until(
-        EC.presence_of_element_located((By.XPATH, '//iframe[@title="reCAPTCHA"]')))
-    driver.switch_to.frame(recaptcha_iframe1)
+    recaptcha_iframe = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, iframe_xpath))
+    )
+    driver.switch_to.frame(recaptcha_iframe)
 
 
-def go_to_recaptcha_iframe2(driver):
-    driver.switch_to.default_content()
-    recaptcha_iframe2 = WebDriverWait(driver=driver, timeout=10).until(
-        EC.presence_of_element_located((By.XPATH, '//iframe[contains(@title, "challenge")]')))
-    driver.switch_to.frame(recaptcha_iframe2)
-
-
-def get_target_num(driver):
-    target = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-        (By.XPATH, '//div[@id="rc-imageselect"]//strong')))
-
+def get_target_num(target_text):
     target_mapping = {
-        "bicycle": 1,
-        "bus": 5,
-        "tractor": 7,
-        "boat": 8,
-        "car": 2,
-        "hydrant": 10,
+        "bicycle"   : 1,
+        "bus"       : 5,
+        "tractor"   : 7,
+        "boat"      : 8,
+        "car"       : 2,
+        "hydrant"   : 10,
         "motorcycle": 3,
-        "traffic": 9,
-        "crosswalk": 1001,
-        "stair": 1002,
-        "taxi": 1003
+        "traffic"   : 9,
+        "crosswalk" : 1001,
+        "stair"     : 1002,
+        "taxi"      : 1003
     }
 
     for key, value in target_mapping.items():
-        if re.search(key, target.text) is not None:
+        if re.search(key, target_text) is not None:
             return value
 
     return 1000
@@ -74,16 +69,16 @@ def get_answers(target_num, timestamp):
     image = np.asarray(image)
 
     if target_num == 1001:
-        result = crosswalk_model.predict(image)
+        result = yolo_models["crosswalk"].predict(image)
         target_num = 0
     elif target_num == 1002:
-        result = yolo_oiv7_model.predict(image)
+        result = yolo_models["yolov8x-oiv7"].predict(image)
         target_num = 489
     elif target_num == 1003:
-        result = yolo_oiv7_model.predict(image)
+        result = yolo_models["yolov8x-oiv7"].predict(image)
         target_num = 522
     else:
-        result = yolo_model.predict(image)
+        result = yolo_models["yolov8x"].predict(image)
 
     target_index = [i for i, num in enumerate(
         result[0].boxes.cls) if num == target_num]
@@ -138,7 +133,7 @@ def get_all_new_dynamic_captcha_img_urls(answers, before_img_urls, driver):
 
     index_common = []
     for answer in answers:
-        if img_urls[answer-1] == before_img_urls[answer-1]:
+        if img_urls[answer - 1] == before_img_urls[answer - 1]:
             index_common.append(answer)
 
     if len(index_common) >= 1:
@@ -169,11 +164,11 @@ def paste_new_img_on_main_img(main, new, loc, timestamp):
 
 def get_occupied_cells(vertices):
     occupied_cells = set()
-    rows, cols = zip(*[((v-1)//4, (v-1) % 4) for v in vertices])
+    rows, cols = zip(*[((v - 1) // 4, (v - 1) % 4) for v in vertices])
 
-    for i in range(min(rows), max(rows)+1):
-        for j in range(min(cols), max(cols)+1):
-            occupied_cells.add(4*i + j + 1)
+    for i in range(min(rows), max(rows) + 1):
+        for j in range(min(cols), max(cols) + 1):
+            occupied_cells.add(4 * i + j + 1)
 
     return sorted(list(occupied_cells))
 
@@ -211,7 +206,7 @@ def get_answers_4(target_num, timestamp):
     image = np.asarray(image)
 
     if target_num < 1000:
-        result_seg = yolo_seg_model.predict(image)
+        result_seg = yolo_models["yolov8x-seg"].predict(image)
 
         target_index = []
         count = 0
@@ -246,13 +241,13 @@ def get_answers_4(target_num, timestamp):
         return list(set(answers))
     else:
         if target_num == 1001:
-            result = crosswalk_model.predict(image)
+            result = yolo_models["crosswalk"].predict(image)
             target_num = 0
         elif target_num == 1002:
-            result = yolo_oiv7_model.predict(image)
+            result = yolo_models["yolov8x-oiv7"].predict(image)
             target_num = 489
         elif target_num == 1003:
-            result = yolo_oiv7_model.predict(image)
+            result = yolo_models["yolov8x-oiv7"].predict(image)
             target_num = 522
 
         boxes = result[0].boxes.data
@@ -264,19 +259,11 @@ def get_answers_4(target_num, timestamp):
                 target_index.append(count)
             count += 1
 
-        for i in target_index:
-            target_box = boxes[i]
-            p1, p2 = (int(target_box[0]), int(target_box[1])
-                      ), (int(target_box[2]), int(target_box[3]))
-            x1, y1 = p1
-            x2, y2 = p2
-
         answers = []
-        count = 0
         for i in target_index:
             target_box = boxes[i]
-            p1, p2 = (int(target_box[0]), int(target_box[1])
-                      ), (int(target_box[2]), int(target_box[3]))
+            p1, p2 = (int(target_box[0]), int(target_box[1])), (int(
+                target_box[2]), int(target_box[3]))
             x1, y1 = p1
             x4, y4 = p2
             x2 = x4
@@ -317,12 +304,12 @@ def get_answers_4(target_num, timestamp):
 
 
 def solve_recaptcha(driver):
-    go_to_recaptcha_iframe1(driver)
+    go_to_recaptcha_iframe(driver, '//iframe[@title="reCAPTCHA"]')
 
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
         (By.XPATH, '//div[@class="recaptcha-checkbox-border"]'))).click()
 
-    go_to_recaptcha_iframe2(driver)
+    go_to_recaptcha_iframe(driver, '//iframe[contains(@title, "challenge")]')
 
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     random_num = random.randint(100000, 999999)
@@ -336,10 +323,12 @@ def solve_recaptcha(driver):
                 title_wrapper = WebDriverWait(driver, 10).until(
                     EC.presence_of_element_located((By.ID, 'rc-imageselect')))
 
-                target_num = get_target_num(driver)
+                target_text = title_wrapper.find_element(
+                    By.XPATH, './/strong').text
+                target_num = get_target_num(target_text)
 
                 if target_num == 1000:
-                    print("skipping")
+                    print("Skipping")
                     reload.click()
                 elif "squares" in title_wrapper.text:
                     print("Square captcha found....")
@@ -352,9 +341,9 @@ def solve_recaptcha(driver):
                     else:
                         reload.click()
                 elif "none" in title_wrapper.text:
-                    print("found a 3x3 dynamic captcha")
+                    print("Found a 3x3 dynamic captcha")
                     img_urls = get_all_captcha_img_urls(driver)
-                    if len(set(list(img_urls))) == 1:
+                    if len(set(img_urls)) == 1:
                         download_img(0, img_urls[0], timestamp)
                         answers = get_answers(target_num, timestamp)
                         if len(answers) > 2:
@@ -365,7 +354,7 @@ def solve_recaptcha(driver):
                     else:
                         reload.click()
                 else:
-                    print("found a 3x3 one time selection captcha")
+                    print("Found a 3x3 one-time selection captcha")
                     img_urls = get_all_captcha_img_urls(driver)
                     download_img(0, img_urls[0], timestamp)
                     answers = get_answers(target_num, timestamp)
@@ -391,11 +380,10 @@ def solve_recaptcha(driver):
 
                     new_img_index_urls = []
                     for answer in answers:
-                        new_img_index_urls.append(answer-1)
-                    new_img_index_urls
+                        new_img_index_urls.append(answer - 1)
 
                     for index in new_img_index_urls:
-                        download_img(index+1, img_urls[index], timestamp)
+                        download_img(index + 1, img_urls[index], timestamp)
                     while True:
                         try:
                             for answer in answers:
@@ -415,11 +403,11 @@ def solve_recaptcha(driver):
                                     break
                             new_img_index_urls = []
                             for answer in answers:
-                                new_img_index_urls.append(answer-1)
+                                new_img_index_urls.append(answer - 1)
 
                             for index in new_img_index_urls:
                                 download_img(
-                                    index+1, img_urls[index], timestamp)
+                                    index + 1, img_urls[index], timestamp)
 
                     answers = get_answers(target_num, timestamp)
 
@@ -440,29 +428,34 @@ def solve_recaptcha(driver):
 
             for i in range(200):
                 try:
-                    go_to_recaptcha_iframe2(driver)
+                    go_to_recaptcha_iframe(
+                        driver, '//iframe[contains(@title, "challenge")]')
                     WebDriverWait(driver, 0.1).until(
-                        EC.presence_of_element_located((By.XPATH, '//button[@id="recaptcha-verify-button" and not(contains(@class, "rc-button-default-disabled"))]')))
+                        EC.presence_of_element_located(
+                            (By.XPATH, '//button[@id="recaptcha-verify-button" and not(contains(@class, "rc-button-default-disabled"))]'))
+                    )
                     solved = False
                     break
                 except:
-                    go_to_recaptcha_iframe1(driver)
+                    go_to_recaptcha_iframe(
+                        driver, '//iframe[@title="reCAPTCHA"]')
                     if WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//span[@id="recaptcha-anchor"]'))).get_attribute("aria-checked") == 'true':
                         solved = True
                         break
                     else:
                         solved = False
             if solved:
-                print("solved")
+                print("Solved")
                 list_images_directory = listdir(images_directory)
                 for image in list_images_directory:
-                    if re.search(timestamp, image) != None:
+                    if re.search(timestamp, image) is not None:
                         remove(images_directory.joinpath(image))
 
                 driver.switch_to.default_content()
                 break
             else:
-                go_to_recaptcha_iframe2(driver)
+                go_to_recaptcha_iframe(
+                    driver, '//iframe[contains(@title, "challenge")]')
 
         except Exception as e:
             print(e)
